254a371a5c121cc7d1491c5666ccd939314a7c1f
hir1m <vanilia.miring@gmail.com>
2021-04-02 00:09:54 -0700

    test ssl

--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,7 @@ const connectionUri = process.env.DATABASE_URL;
 // connect with db
 const pool = new Pool({
   connectionString: connectionUri,
+  ssl: process.env.NODE_ENV === "production" ? true : false,
 });
 
 export default pool;
